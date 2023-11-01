8f44f0e2ab436412c0351468c6396c3d086ad3e3
implodingduck <zsek320@gmail.com>
2023-11-01 15:11:45 -0500

    updating outputs

--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,20 @@
 output "result" {
   value = {
     values = {
-      server   = ""
-      port     = ""
-      username = ""
-      database = ""
+      server   = azurerm_mssql_server.db.fully_qualified_domain_name
+      port     = "1433"
+      username = azurerm_mssql_server.db.administrator_login
+      database = azurerm_mssql_database.db.name
     }
     secrets = {
-      password = ""
+      password = azurerm_mssql_server.db.administrator_login_password
       connectionString = ""
     }
     // UCP resource IDs
     resources = [
-        ""
+        "/planes/azure/azurecloud/${azurerm_resource_group.rg.id}",
+        "/planes/azure/azurecloud/${azurerm_mssql_server.db.id}",
+        "/planes/azure/azurecloud/${azurerm_mssql_database.db.id}",
     ]
   }
   description = "The result of the Recipe. Must match the target resource's schema."
